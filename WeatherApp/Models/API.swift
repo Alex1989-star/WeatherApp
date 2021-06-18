875f2a1bef66c16d02832ae7380e8c73b716b977
Falikor <Bugorv90@mail.ru>
2021-06-18 23:07:03 +0300

    added model and method

--- WeatherApp/Models/API.swift
+++ WeatherApp/Models/API.swift
@@ -7,30 +7,44 @@
 
 import Foundation
 
-func searchBarSearchButtonClicked(searchBar: String) {
-        let urlString = "https://api.weatherapi.com/v1/current.json?key=1f71ab5474014f50867131317211806&q=\(searchBar)&aqi=yes"
+class WeatherCurrently {
+    
+    var city: String?
+    var tempC: Double?
+    var icon: String?
+    var textHowWeather: String?
+    var isDay: Int? // при 0 ночь при 1 день
+    
+    init(wetherJson: NSDictionary) {
+        let location = wetherJson["location"] as! NSDictionary
+        city = location["name"] as! String
+        let current = wetherJson["current"] as! NSDictionary
+        tempC = current["temp_c"] as! Double
+        isDay = current["is_day"] as! Int
+        let condition = current["condition"] as! NSDictionary
+        icon = condition["icon"] as! String
+        textHowWeather = condition["text"] as! String
+    }
+    
+    func weatherInCity(city: String) {
+        let urlString = "https://api.weatherapi.com/v1/current.json?key=1f71ab5474014f50867131317211806&q=\(city)&aqi=yes"
         let url = URL(string: urlString)
-        
-        var locationName: String?
-        var temperature: Double?
-        
         let task = URLSession.shared.dataTask(with: url!) {(data, response, error) in
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
-                if let location = json["location"] {
-                    locationName = location["name"] as? String
-                    print(locationName)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! NSDictionary
+                    let weather = WeatherCurrently(wetherJson: json)
+                    print(weather.city!)
+                    print(weather.tempC!)
+                    print(weather.icon!)
+                    print(weather.textHowWeather!)
+                    print(weather.isDay!)
                 }
-                if let current = json["current"] {
-                    temperature = current["temp_c"] as? Double
-                    print(temperature)
+                catch let jsonError {
+                    print(jsonError)
                 }
-            }
-            catch let jsonError {
-                print(jsonError)
-            }
 
-        }
-        task.resume()
+            }
+            task.resume()
+    }
 }
 
