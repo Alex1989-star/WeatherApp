83fab899e903955fc5ee2afb8967dca8b61de106
Sergey Vorobev <rusbear28@yandex.ru>
2021-06-19 17:45:13 +0700

    Added network layer and models

---
+++ WeatherApp/Models/Weather.swift
@@ -0,0 +1,49 @@
+//
+//  Weather.swift
+//  WeatherApp
+//
+
+import Foundation
+
+// MARK: - Weather
+struct Weather: Codable {
+    let weather: [WeatherElement]
+    let main: Main
+    let visibility: Int
+    // let wind: Wind
+    let clouds: Clouds
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp, feelsLike, tempMin, tempMax: Double
+    let pressure, humidity: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
+    }
+}
+
+// MARK: - WeatherElement
+struct WeatherElement: Codable {
+    let id: Int
+    let main, weatherDescription, icon: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, main
+        case weatherDescription = "description"
+        case icon
+    }
+}
